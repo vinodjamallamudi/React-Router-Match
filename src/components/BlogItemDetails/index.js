02d50c87ee8e1086fe140a632309b50a82f090f1
vinod <vvinnu812@gmail.com>
2024-10-01 07:10:04 +0000

    first commit

---
+++ src/components/BlogItemDetails/index.js
@@ -0,0 +1,52 @@
+import {Component} from 'react'
+
+import './index.css'
+
+class BlogItemDetails extends Component {
+  state = {blogData: {}}
+
+  componentDidMount() {
+    this.getBlogItemData()
+  }
+
+  getBlogItemData = async () => {
+    const {match} = this.props
+    const {params} = match
+    const {id} = params
+    const response = await fetch(`https://apis.ccbp.in/blogs/${id}`)
+    const data = await response.json()
+    const updatedData = {
+      imageUrl: data.image_url,
+      title: data.title,
+      avatarUrl: data.avatar_url,
+      topic: data.topic,
+      content: data.content,
+      id: data.id,
+    }
+    this.setState({blogData: updatedData})
+  }
+
+  renderBlogItemDetails = () => {
+    const {blogData} = this.state
+    const {title, imageUrl, content, avatarUrl, author} = blogData
+    return (
+      <div className="blog-info">
+        <h2 className="blog-details-title">{title}</h2>
+
+        <div className="author-details">
+          <img className="author-pic" src={avatarUrl} alt={author} />
+          <p className="details-author-name">{author}</p>
+        </div>
+
+        <img className="blog-image" src={imageUrl} alt={title} />
+        <p className="blog-content">{content}</p>
+      </div>
+    )
+  }
+
+  render() {
+    return <div className="blog-container">{this.renderBlogItemDetails()}</div>
+  }
+}
+
+export default BlogItemDetails
